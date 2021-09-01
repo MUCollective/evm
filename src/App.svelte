358f0ad6dacc443b085c60f1d5686457525089b4
Emily Qiao <emilyqiao@Emilys-MacBook-Pro.local>
2021-08-31 21:57:13 -0500

    keep working hehe

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script>
 	import Header from "./Header.svelte";
-	let name = "mimily";
+	let name = "exploratory modeling";
 </script>
 
 <main>
 	<div class="header">
 		<Header/>
 	</div>
-	<h1>Hello {name}?</h1>
+	<h1>{name}!</h1>
 	<p>
 		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
 		how to build Svelte apps.
