f83bd8777cd7546279b3b60a62b6be62a47e8c82
Alex Kale <kalea@uw.edu>
2021-09-12 11:12:26 -0500

    fixed drop zones so that they work as intended and demonstrate a scaleable design pattern where UI elements are generated programmatically

--- src/App.svelte
+++ src/App.svelte
@@ -6,120 +6,194 @@
 	import { Grid, Row, Column } from "carbon-components-svelte";
 	import { onMount } from "svelte";
 	import * as d3 from "d3";
-import { filter } from "d3";
 
-	// import * as cars from './data/cars.json'
-
-	// $: cars && console.log(cars);
 
 	let data = null,
-		items = null,
-		curDragIdx = null;
+		mounted = false;
+		// items = null,
+		// curDragIdx = null,
+		// curDropId = null;
 
-
-	// export let name = null;
+	// TODO: generate drop zones from this spec programmatically
+	let dndState = [
+		{
+			id: "variables",
+			name: "Dataset variables (drag zone)",
+			items: []
+		},
+		{
+			id: "x-drop",
+			name: "Horizontal position (drop zone)",
+			items: []
+		},
+		{
+			id: "y-drop",
+			name: "Vertical position (drop zone)",
+			items: []
+		}
+	];
 
 	onMount(async () => {
 		// load data
 		data = await d3.json("./data/cars.json");
 		console.log('loaded data', data);
 		// populate dropzone items with variable names from data
-		items = [];
+		// items = [];
+		// Object.keys(data[0]).forEach((d, i) => {
+		// 	items.push({ 
+		// 		id: i, // gets overwritten by drag consider
+		// 		idx: i, 
+		// 		name: d,
+		// 		shelf: 'variables' // start on variables shelf
+		// 	});
+		// });
 		Object.keys(data[0]).forEach((d, i) => {
-			items.push({ 
+			dndState[0].items.push({ 
 				id: i, // gets overwritten by drag consider
 				idx: i, 
-				name: d,
-				shelf: 'variables' // start on variables shelf
+				name: d
 			});
 		});
-		console.log('drop zone items', items);
+		console.log('drop zone state', dndState);
+		mounted = true;
 	});
 
-	// let items = [
-	// 	{ id: 1, name: "item1" },
-	// 	{ id: 2, name: "item2" },
-	// 	{ id: 3, name: "item3" },
-	// 	{ id: 4, name: "item4" },
-	// ];
 	const flipDurationMs = 300;
-	function handleDndConsider(e) {
-		// console.log(e);
-		items = e.detail.items; // overwriting items here may lead to items getting dropped once they are no longer in the drag zone; the issue may actually be in the filtering of items below
-		// get idx for dragged variable
-		curDragIdx = e.detail.items.filter((d) => (d.hasOwnProperty('isDndShadowItem') && d.isDndShadowItem))[0].idx;
-	}
-	function handleDndFinalize(e) {
+	function handleDndConsider(shelfId, e) {
 		console.log(e);
-		items = e.detail.items;
-		// reset shelf for current variable
-		// TODO: get shelf name from event handler
-		let cur = items.filter((d) => d.idx === curDragIdx);
-		cur[0].shelf = 'x-drop'; // hardcoded for now
-		console.log(cur);
+		const shelfIdx = dndState.findIndex(d => d.id === shelfId);
+		dndState[shelfIdx].items = e.detail.items;
+		dndState = [...dndState];
+		// if (e.detail.info.trigger === "draggedEntered") {
+		// 	// get idx for dragged variable
+		// 	curDragIdx = e.detail.items.filter((d) => (d.hasOwnProperty('isDndShadowItem') && d.isDndShadowItem))[0].idx;
+		// 	// get shelf name from event handler
+		// 	curDropId = e.srcElement.id;
+		// }
+	}
+	function handleDndFinalize(shelfId, e) {
+		console.log(e);
+		const shelfIdx = dndState.findIndex(d => d.id === shelfId);
+		dndState[shelfIdx].items = e.detail.items;
+		dndState = [...dndState];
+		// // reset shelf for current variable
+		// let cur = items.filter((d) => d.idx === curDragIdx);
+		// cur[0].shelf = curDropId;
+		// console.log(cur);
 	}
 </script>
 
 <main>
 	<Header />
 	<Grid fullWidth>
-		<Row>
-			<Column style="outline: 1px solid var(--cds-interactive-04)">
-				Dataset variables (drag zone)
-				{#if items}
+		{#if mounted}
+			<Row>
+				<!-- TODO:
+					+ add Vertical position (drop zone) with it's own shelf id ('y-drop')
+					+ create a json state that controls what renders on all of the shelves, keeping track of items across shelves
+					+ programmatically generate drop zones from a json spec
+					+ figure out why drop zones don't work, why trigger is always "droppedOutsideOfAny" (problem was that drop zones had no height; added css below to fix)
+					- link Chart to variables in drop zone
+					- format drop zones and draggable blocks similar to Polestar
+					- use css to modify layout to roughly match Polestar, leaving room for the modeling sidebar on the right
+				-->
+				<Column style="outline: 1px solid var(--cds-interactive-04)">
+					{dndState[0].name}
 					<section
-						use:dndzone={{items: items.filter((d) => d.shelf === 'variables'), flipDurationMs }}
-						on:consider={handleDndConsider}
-						on:finalize={handleDndFinalize}
+						use:dndzone={{items: dndState[0].items, flipDurationMs }}
+						on:consider={(e) => handleDndConsider(dndState[0].id, e)}
+						on:finalize={(e) => handleDndFinalize(dndState[0].id, e)}
+						id={dndState[0].id}
 					>
-						{#each items.filter((d) => d.shelf === 'variables') as item (item.id)}
+						{#each dndState[0].items as item (item.id)}
 							<div animate:flip={{ duration: flipDurationMs }}>
 								{item.name}
 							</div>
 						{/each}
 					</section>
-				{/if}
-			</Column>
-			<!-- TODO: 
-				* fix drop zone callbacks so that overwriting the variable in drop zone sends variable that was previously there back to the drag zone
-				* add Vertical position encoding (drop zone) with it's own shelf property in items (i.e., use:dndzone={{items: items.filter((d) => d.shelf === 'y-drop'...))
-				* format drop zones and draggable blocks like Polestar
-				* link Chart to variables in drop zone
-			-->
-			<Column style="outline: 1px solid var(--cds-interactive-04)">
-				Horizontal position encoding (drop zone)
-				{#if items}
-					<section
-						use:dndzone={{ items: items.filter((d) => d.shelf === 'x-drop'), flipDurationMs }}
-						on:consider={handleDndConsider}
-						on:finalize={handleDndFinalize}
-					>
-						{#each items.filter((d) => d.shelf === 'x-drop') as item (item.id)}
-							<div animate:flip={{ duration: flipDurationMs }}>
-								{item.name}
-							</div>
-						{/each}
-					</section>
-				{/if}
-			</Column>
-
-			<Column style="outline: 1px solid var(--cds-interactive-04)"
-				>chart zone
-				<!-- <section
-					use:dndzone={{ items, flipDurationMs }}
-					on:consider={handleDndConsider}
-					on:finalize={handleDndFinalize}
-				>
-					{#each items as item (item.id)}
-						<div animate:flip={{ duration: flipDurationMs }}>
-							{item.name}
-						</div>
+				</Column>
+				<Column>
+					{#each dndState as shelf}
+						{#if shelf.id !== 'variables'}
+							<Row style="padding: 20px; height: 150px;">
+								<Column style="outline: 1px solid var(--cds-interactive-04);">
+									{shelf.name}
+									<section
+										use:dndzone={{ items: shelf.items, flipDurationMs }}
+										on:consider={(e) => handleDndConsider(shelf.id, e)}
+										on:finalize={(e) => handleDndFinalize(shelf.id, e)}
+										id={shelf.id}
+										style="height: 100%;"
+									>
+										{#each shelf.items as item (item.id)}
+											<div animate:flip={{ duration: flipDurationMs }}>
+												{item.name}
+											</div>
+										{/each}
+									</section>
+								</Column>
+							</Row>
+						{/if}
 					{/each}
-				</section> -->
-				<Chart />
-				<!-- <p>{cars.mykey}</p> -->
-			</Column>
-		</Row>
+					<!-- <Row style="padding: 20px;">
+						<Column style="outline: 1px solid var(--cds-interactive-04)">
+							Horizontal position (drop zone)
+							{#if items}
+								<section
+									use:dndzone={{ items: items, flipDurationMs }}
+									on:consider={handleDndConsider}
+									on:finalize={handleDndFinalize}
+									id='x-drop'
+								>
+									{#each items.filter((d) => d.shelf === 'x-drop') as item (item.id)}
+										<div animate:flip={{ duration: flipDurationMs }}>
+											{item.name}
+										</div>
+									{/each}
+								</section>
+							{/if}
+						</Column>
+					</Row>
+					
+					<Row style="padding: 20px;">
+						<Column style="outline: 1px solid var(--cds-interactive-04)">
+							Vertical position (drop zone)
+							{#if items}
+								<section
+									use:dndzone={{ items: items, flipDurationMs }}
+									on:consider={handleDndConsider}
+									on:finalize={handleDndFinalize}
+									id='y-drop'
+								>
+									{#each items.filter((d) => d.shelf === 'x-drop') as item (item.id)}
+										<div animate:flip={{ duration: flipDurationMs }}>
+											{item.name}
+										</div>
+									{/each}
+								</section>
+							{/if} 
+						</Column>
+					</Row> -->
+				</Column>
+
+				<Column style="outline: 1px solid var(--cds-interactive-04)"
+					>chart zone
+					<!-- <section
+						use:dndzone={{ items, flipDurationMs }}
+						on:consider={handleDndConsider}
+						on:finalize={handleDndFinalize}
+					>
+						{#each items as item (item.id)}
+							<div animate:flip={{ duration: flipDurationMs }}>
+								{item.name}
+							</div>
+						{/each}
+					</section> -->
+					<Chart />
+					<!-- <p>{cars.mykey}</p> -->
+				</Column>
+			</Row>
+		{/if}
 	</Grid>
 </main>
 
